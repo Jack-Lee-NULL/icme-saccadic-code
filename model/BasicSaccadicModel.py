d98999cd7051b88f9afa24739e2fd4ebea7da3e6
jqLee <m13152143642@163.com>
2019-03-07 17:16:26 +0800

    create some basic files, move BasicConvLSTMCell in

---
+++ model/BasicSaccadicModel.py
@@ -0,0 +1,14 @@
+#
+# file: BasicSaccadicModel.py
+# author: Jingquan Lee
+# date: 2019-03-07
+#
+#
+
+class BasicSaccadicModel:
+
+    def __init__():
+        pass
+
+    def __call__():
+        pass
---
+++ model/__init__.py
@@ -0,0 +1,12 @@
+#
+# file: __init__.py
+# author: Jingquan Lee
+# date: 2019-03-07
+#
+#
+
+import os
+import sys
+
+
+__all__ = ['BasicSaccadicModel', 'BasicConvLSTMCell']
