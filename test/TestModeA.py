603c0e9398be0bf64b940c9d58b559bd71c42fc6
jqLee <m13152143642@163.com>
2019-03-10 15:35:34 +0800

    created TestModeA.py and ./test/__init__.py

---
+++ test/TestModeA.py
@@ -0,0 +1,18 @@
+#
+#
+# file: TestModeA.py
+# author: Jingquan Lee
+# date: 2019-03-10
+#
+#
+
+class TestModeA:
+
+    def __init__(self):
+        pass
+
+    def predicts(self):
+        pass
+
+    def decode_preds(self):
+        pass
